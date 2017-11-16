ca0ba7a55426e39c06db98c627d853b42ca2c9ac
Matthias Babel <MatthiasBabel@Matthias-MBP.fritz.box>
2017-11-16 15:59:15 +0100

    Anfang

---
+++ cb/parser/backtracking/BacktrackingParser.java
@@ -0,0 +1,14 @@
+
+package cb.parser.backtracking
+
+import uni_bayreuth.ai6.exercises.submission.ISubmission
+import uni bayreuth.ai6.compiler.exercises.interfaces.SyntaxAnalysis
+
+public class BacktrackingParser implements ISubmission, SyntaxAnalysis {
+
+    public BacktrackingParser() {
+    }
+    public static Scanner get Intance() {
+        return new BacktrackingParser()
+    }
+}
